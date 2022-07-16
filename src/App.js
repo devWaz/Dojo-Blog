d818e5b6e246ed131de177e9c51d239b9ff83623
devWaz <fawazodessy@gmail.com>
2022-07-16 14:25:33 +0100

    Routing Parameters

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from './navbar'
 import HomePage from './Home' 
 import {BrowserRouter as Router , Route , Switch} from 'react-router-dom';
 import Create from './create';
+import BlogDetails from './blogDetails';
 
 function App() {
 
@@ -11,12 +12,19 @@ function App() {
         <Navbar/>
         <div className='content'>
           <Switch>
+
             <Route exact path="/">
               <HomePage/>
             </Route>
+
             <Route path="/create">
               <Create/>
             </Route>
+
+            <Route path="/blogs/:id">
+              <BlogDetails/>
+            </Route>
+
           </Switch>
         </div>
       </div>
