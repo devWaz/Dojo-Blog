cb330ca9e3145c57578d8d0c112f63d72271c470
devWaz <fawazodessy@gmail.com>
2022-07-17 23:39:39 +0100

    Controlled Inputs

--- src/create.js
+++ src/create.js
@@ -1,7 +1,41 @@
+import { useState } from "react";
+
 const Create = () => {
-    return ( 
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('')
+
+    return (
         <div className="create">
             <h2>Add a new blog</h2>
+            <form>
+                <label>Blog Title:</label>
+                <input type="text" 
+                        value={title} 
+                        onChange = {(e) => {setTitle(e.target.value)}} 
+                        required/>
+
+                <label>Blog Body:</label>
+                <textarea required
+                            value={body} 
+                            onChange = {(e) => {setBody(e.target.value)}}
+                            ></textarea>
+
+                <label>Blog Author:</label>
+                <select 
+                        value={author}
+                        onChange= {(e) => setAuthor(e.target.value)}>
+                    <option value="mario">mario</option>
+                    <option value="yoshi">yoshi</option>
+                    <option value="devWaz">devWaz</option>
+                </select>
+
+                <button>Add Blog</button>
+            </form>
+
+            <p>{title}</p>
+            <p>{body}</p>
+            <p>{author}</p>
         </div>
      );
 }
