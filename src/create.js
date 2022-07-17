4308f7ea492d2f085dc5d03ce54137954886a9b4
devWaz <fawazodessy@gmail.com>
2022-07-17 23:56:10 +0100

    Submit Events

--- src/create.js
+++ src/create.js
@@ -3,12 +3,19 @@ import { useState } from "react";
 const Create = () => {
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
-    const [author, setAuthor] = useState('')
+    const [author, setAuthor] = useState('');
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        const blog = {title , body , author};
+
+        console.log(blog)
+    }
 
     return (
         <div className="create">
             <h2>Add a new blog</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Blog Title:</label>
                 <input type="text" 
                         value={title} 
